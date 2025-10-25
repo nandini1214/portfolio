c593c6842446a7262f29a8add10a14447089aa2c
freelancerwe2code <99020171+nandini1214@users.noreply.github.com>
2025-10-25 14:18:38 +0530

    prepare for the final deployement

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home.tsx'
-import About from './pages/About.tsx'
-import Projects from './pages/Projects.tsx'
-import Contact from './pages/Contact.tsx'
+
 import AddProject from './pages/AddProject.tsx'
 
 createRoot(document.getElementById('root')!).render(
